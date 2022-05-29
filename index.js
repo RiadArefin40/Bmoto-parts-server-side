03d2009fd485412662f3732ec77ab45db0e861a7
RiadArefin <arefinriad40@gmail.com>
2022-05-29 13:14:27 +0600

    first commit

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run(){
             res.send(orders)
             
         })
+        app.get('/order/:id',async(req,res)=>{
+            const  id = req.params.id
+            
+            const query = {_id: ObjectId(id)};
+            const order = await orderCOllection.findOne(query)
+            res.send(order)
+            
+        })
 
         app.get('/users',async(req,res)=>{
             const users = await  userCollection.find().toArray();
